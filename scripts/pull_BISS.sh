43db3a64be63d9f68cc7cb0aee1b4ae436c7dae3
dezingaro <28324421+dezingaro@users.noreply.github.com>
2023-01-30 15:12:15 +0100

    Update pull_BISS.sh

--- scripts/pull_BISS.sh
+++ scripts/pull_BISS.sh
@@ -2,20 +2,21 @@
 
 root="/home/projects/BISS"
 repository="https://github.com/dezingaro/BISS.git"
-git ls-remote "$repository" "refs/heads/*" | while read sha ref; do
-  branch=$(basename "$ref")
-  if [[ $branch -ne "master" ]]; then
-    target="$root/public_html/$branch"
-    if [[ -d "$target" ]]; then
+git ls-remote "$repository" "refs/heads/*" | while read sha ref; do branch=$(basename "$ref")
+
+if [[ $branch -ne "master" ]]; then
+  target="$root/public_html/$branch"
+  if [[ -d "$target" ]]; then
     cd "$target"
     git pull
-    else
-    git clone --depth=1 https://github.com/dezingaro/BISS.git --single-branch --branch "$branch" "$target"
+  else
+    git clone --depth=1 $repository --single-branch --branch "$branch" "$target"
     cd "$target"
     git config core.sharedRepository true
     cd "$root"
-    fi
-    chmod -R 775 "$target"
-    # chgrp -R biss "$target"
   fi
+  chmod -R 775 "$target"
+  # chgrp -R biss "$target"
+fi
+
 done
